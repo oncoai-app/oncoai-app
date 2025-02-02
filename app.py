eb66a4dbc3499dc5628ac60a49fcc7a83bb38a86
oncoai-app <oncoaiceo@gmail.com>
2025-02-02 17:00:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ COLORS = {
 # Sidebar for disease selection
 with st.sidebar:
     st.write("**Created By:** Kavin Elangovan")
-    st.header("Select Disease")
+    #st.header("Select Disease")
     
     selected_disease = st.selectbox("Choose a disease to analyze:", list(DISEASE_CONFIGS.keys()))
 
