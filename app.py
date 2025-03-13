3a41f843cab42c4844fe50adfaba4cde568c5500
oncoai-app <oncoaiceo@gmail.com>
2025-03-12 20:55:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ COLORS = {
 
 # Sidebar for disease selection
 with st.sidebar:
-    st.link_button("Explore **Onco-AIDE**", "https://oncoai.org/aide")
+    st.link_button("Explore **OncoAIDE**", "https://oncoai.org/aide")
     st.divider()
     st.write("**Created By:** Kavin Elangovan")
     st.write("**Model:** EfficientNetB0")
